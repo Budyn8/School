ec094dcc659d5cae865df960470ee1965eacf4e4
Budzyński <m.budzyski.2019@t9l.local>
2023-09-28 08:13:29 +0200

    creating an python create folder file (python)

---
+++ Python/main.py
@@ -0,0 +1,19 @@
+import datetime
+import os
+
+
+def creating_folder():
+
+    main_path = r'C:\xampp\htdocs\5ti\School\Paibd'
+    folder_name = str(datetime.date.today())
+    path = os.path.join(main_path, folder_name)
+    if os.path.exists(path):
+        return 'Plik już istnieje'
+
+    os.mkdir(path)
+    return 'Plik został stworzony'
+
+
+if __name__ == '__main__':
+
+    print(creating_folder())
